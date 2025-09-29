9a0759d931f1b3e3a28290b04d9f49ff74ef8f8b
Mohamed Yansane <yansane472@gmail.com>
2025-09-29 00:00:15 +0000

    09: widget layout

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,24 +1,15 @@
 "use client"
-import { Button } from "@workspace/ui/components/button"
-import { useMutation} from "convex/react"
-import { api } from "@workspace/backend/_generated/api"
-import { useVapi } from "@/modules/widget/hooks/use-vapi"
-export default function Page() {
-  const {startCall, endCall, isConnected, isConnecting, isSpeaking, transcript} = useVapi()
-  const addUser = useMutation(api.users.add);
-  return (
-    <div className="flex items-center justify-center min-h-svh max-w-md w-full mx-auto">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <Button onClick={() => startCall()}>Start Call</Button>
-        <Button variant="destructive" onClick={() => endCall()}>
-          End Call
-        </Button>
-        <p>isConnected: {`${isConnected}`}</p>
-        <p>isConnecting: {`${isConnecting}`}</p>
-        <p>isSpeaking: {`${isSpeaking}`}</p>
-        <p>{JSON.stringify(transcript, null, 2)}</p>
-        <Button onClick={() => addUser()}>Add User</Button>
-      </div>
-    </div>
-  );
+
+import { WidgetView } from "@/modules/widget/ui/views/widget-view";
+import { use } from "react";
+
+interface PageProps {
+  searchParams:Promise<{ organizationId: string }>;
 }
+const Page = ({ searchParams }: PageProps) => {
+  const { organizationId } = use(searchParams);
+  //comme le widget view utilisera forcement les hooks use state ben la page devient client component et du coup au lieu de await params jutilise use from react
+  if (!organizationId) return <div>No organizationId</div>;
+  return <WidgetView organizationId={organizationId} />;
+};
+export default Page;
---
+++ apps/widget/modules/widget/ui/components/widget-footer.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Button } from "@workspace/ui/components/button";
+import { cn } from "@workspace/ui/lib/utils";
+import { HomeIcon, InboxIcon } from "lucide-react";
+import { useState } from "react";
+
+export const WidgetFooter = () => {
+    const [screen, setSelectedScreen] = useState<"selection" | "inbox">("selection")
+    return (
+    <footer className="flex items-center justify-between border-t bg-background">
+      <Button
+        className="h-14 flex-1 rounded-none"
+        onClick={() => setSelectedScreen("selection")}
+        size="icon"
+        variant="ghost"
+      >
+        <HomeIcon
+          className={cn("size-5", screen === "selection" && "text-primary")}
+        />
+      </Button>
+      <Button
+        className="h-14 flex-1 rounded-none"
+        onClick={() => setSelectedScreen("inbox")}
+        size="icon"
+        variant="ghost"
+      >
+        <InboxIcon
+          className={cn("size-5", screen === "inbox" && "text-primary")}
+        />
+      </Button>
+    </footer>
+  );
+};
